199abd45e419c14a308221f3d80889b9ed913981
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-04-11 18:02:54 +0100

    complete first section

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,93 @@
+def return_10()
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5 
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  else
+    return "wrong"
+  end
+end
+
+  def number_to_short_month_name(number)
+    case number
+    when 1
+      return "Jan"
+    when 2
+      return "Feb"
+    when 3
+      return "Mar"
+    when 4
+      return "Apr"
+    when 5 
+      return "May"
+    when 6
+      return "Jun"
+    when 7
+      return "Jul"
+    when 8
+      return "Aug"
+    when 9
+      return "Sep"
+    when 10
+      return "Oct"
+    when 11
+      return "Nov"
+    when 12
+      return "Dec"
+    else
+      return "wrong"
+    end
+  end
